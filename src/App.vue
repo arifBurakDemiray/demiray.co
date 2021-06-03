57bca656d57f32389d6afd0426a841b62814aea2
arifBurakDemiray <burakdemiray09@hotmail.com>
2021-06-03 20:08:00 +0300

    feat: construction message page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div className="bg-demiray h-screen flex w-screen justify-center">
+    <div className="color-d-f justify-center text-center pt-12 text-2xl font-bold">
+      <Construction msg="This site is under construction" />
+    </div>
+    <footer className="absolute color-d-f self-end pb-4">
+      Copyright © Arif Burak DEMIRAY 2021
+    </footer>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Construction from "./components/Construction.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Construction
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Construction.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="Construction">
+    <h1>{{ msg }}</h1>
+    <div class="fingerprint-spinner overflow-hidden p-2 w-32 h-32">
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Construction",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.fingerprint-spinner,
+.fingerprint-spinner * {
+  box-sizing: border-box;
+}
+
+.fingerprint-spinner .spinner-ring {
+  position: absolute;
+  border-radius: 75%;
+  border: 2px solid transparent;
+  border-top-color: #2ad57e;
+  animation: fingerprint-spinner-animation 2000ms cubic-bezier(0.99, -1.01, 0.99, 0.05) infinite
+    forwards;
+  margin: auto;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+}
+
+.fingerprint-spinner .spinner-ring:nth-child(1) {
+  height: calc(128px / 9 + 0 * 128px / 9);
+  width: calc(128px / 9 + 0 * 128px / 9);
+  animation-delay: calc(50ms * 1);
+}
+
+.fingerprint-spinner .spinner-ring:nth-child(2) {
+  height: calc(128px / 9 + 1 * 128px / 9);
+  width: calc(128px / 9 + 1 * 128px / 9);
+  animation-delay: calc(50ms * 2);
+}
+
+.fingerprint-spinner .spinner-ring:nth-child(3) {
+  height: calc(128px / 9 + 2 * 128px / 9);
+  width: calc(128px / 9 + 2 * 128px / 9);
+  animation-delay: calc(50ms * 3);
+}
+
+.fingerprint-spinner .spinner-ring:nth-child(4) {
+  height: calc(128px / 9 + 3 * 128px / 9);
+  width: calc(128px / 9 + 3 * 128px / 9);
+  animation-delay: calc(50ms * 4);
+}
+
+.fingerprint-spinner .spinner-ring:nth-child(5) {
+  height: calc(128px / 9 + 4 * 128px / 9);
+  width: calc(128px / 9 + 4 * 128px / 9);
+  animation-delay: calc(50ms * 5);
+}
+
+.fingerprint-spinner .spinner-ring:nth-child(6) {
+  height: calc(128px / 9 + 5 * 128px / 9);
+  width: calc(128px / 9 + 5 * 128px / 9);
+  animation-delay: calc(50ms * 6);
+}
+
+.fingerprint-spinner .spinner-ring:nth-child(7) {
+  height: calc(128px / 9 + 6 * 128px / 9);
+  width: calc(128px / 9 + 6 * 128px / 9);
+  animation-delay: calc(50ms * 7);
+}
+
+.fingerprint-spinner .spinner-ring:nth-child(8) {
+  height: calc(128px / 9 + 7 * 128px / 9);
+  width: calc(128px / 9 + 7 * 128px / 9);
+  animation-delay: calc(50ms * 8);
+}
+
+.fingerprint-spinner .spinner-ring:nth-child(9) {
+  height: calc(128px / 9 + 8 * 128px / 9);
+  width: calc(128px / 9 + 8 * 128px / 9);
+  animation-delay: calc(50ms * 9);
+}
+
+@keyframes fingerprint-spinner-animation {
+  100% {
+    transform: rotate(3128deg);
+  }
+}
+</style>
