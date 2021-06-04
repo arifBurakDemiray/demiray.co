bdda23c3a15c446171b1651318682493dd7c24ce
arifBurakDemiray <burakdemiray09@hotmail.com>
2021-06-04 19:09:33 +0300

    fix: tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div className="bg-demiray h-screen flex w-full justify-center">
-    <Links divClassName="w-14 h-6 absolute flex flex-row  right-4 h-8 m-4" className="p-2" />
+    <Links divClassName="w-14 h-8 absolute flex flex-row  right-4 m-4" className="p-2" />
     <Construction
       className="color-d-f justify-center text-center pt-12 text-2xl font-bold"
       msg="Website Under Construction"
