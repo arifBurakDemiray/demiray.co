5a889d2c27b4698eda81c4c12b92adf313516e72
Arif Burak Demiray <burakdemiray09@hotmail.com>
2023-03-20 11:12:02 +0300

    feat: typewriter effect

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <div className="bg-demiray min-h-screen flex w-full justify-center">
     <div
-      className="color-d-f text-center pt-12 "
+      className="color-d-f text-center pt-12"
     >
-      <span className="font-bold text-4xl">Hi, I am Arif Burak Demiray...<br><br></span>
-      <span className="text-1xl">I am a Software Engineer who is obsessed with '<span className="font-bold italic">Single Responsibility</span>'.
-        I am located in Izmir, Turkey.<br>I am a fast learner when it comes to a new tech or a topic. And, I can understand and solve the problem<br>
-        in a zeus way. I am currently working as a Lead Developer.
-      </span>
+      <div class="typingEffect__line1">
+        <span className="font-bold text-4xl">Hi, I am Arif Burak Demiray...<br><br></span>
+      </div>
+      
+      <div class="typingEffect__line2">
+        <span className="text-1xl">I am a Software Engineer who is obsessed with '<span className="font-bold italic">Single Responsibility</span>'.
+        I am located in Izmir, Turkey.
+        </span>
+      </div>
+      <div class="typingEffect__line3">I am a fast learner when it comes to a new tech or a topic. And, I can understand and solve the problem</div>
+      <div class="typingEffect__line4">in a zeus way. I am currently working as a Lead Developer.</div>
+
       <img
         className="mx-auto pt-10"
         src="../assets/me.png"
@@ -32,3 +39,52 @@ export default {
   }
 };
 </script>
+<style>
+
+.typingEffect__line1{
+  width: 0;
+    overflow: hidden; 
+    white-space: nowrap; 
+  animation: typing 1.5s steps(30) forwards, blink 1s 3;
+}
+
+.typingEffect__line2 {
+  width: 0;
+  overflow: hidden; 
+  white-space: nowrap; 
+  animation: typing 1.5s steps(50) 2s forwards, blink 1s 3s 3;
+}
+
+.typingEffect__line3 {
+  width: 0;
+  overflow: hidden; 
+  white-space: nowrap; 
+  animation: typing 2s steps(60) 4s forwards, blink 3s 5s 3;
+}
+
+.typingEffect__line4 {
+  width: 0;
+  overflow: hidden; 
+  white-space: nowrap; 
+  animation: typing 1.5s steps(40) 6s forwards, blink 6s 7s 3;
+}
+
+/* The typing animation */
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+}
+
+@keyframes blink {
+  0%, 45% {
+    border-color: transparent;
+  }
+  50%, 100% {
+    border-color: white;
+  }
+}
+</style>
