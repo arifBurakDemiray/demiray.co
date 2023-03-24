a510c29e7cf7ab9fabcf159224b22f6215262db8
Arif Burak Demiray <burakdemiray09@hotmail.com>
2023-03-24 11:09:23 +0300

    feat: add tags for SEO

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
-<template> <Home /> </template>
+<template>
+  <head>
+    <title>Arif Burak Demiray</title>
+    <meta name="description" content="This is my personel website that connects all my platforms for a developer like me.">
+    <meta name="keywords" content="backend developer, developer, arif burak demiray, arif, burak, demiray">
+  </head>
+
+  <Home /> 
+</template>
 
 <script>
 import Home from './pages/Home.vue';
